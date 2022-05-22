e7e6c4c0a672b3a8f075c17ab584eeba8e193278
Reed Hambrook <reed.hambrook@gmail.com>
2022-05-22 12:54:42 -0700

    updated tests for refactored utils

--- test/DockerService.test.ts
+++ test/DockerService.test.ts
@@ -1,5 +1,7 @@
-import { DockerService, runExec} from "../lib";
+import { DockerService, runExec, waitForTotalExecLoad} from "../lib";
 import { randomUUID } from 'crypto';
+
+jest.setTimeout(30000);
 
 test("Container service", async () => {
     const uuid = randomUUID();
@@ -24,10 +26,11 @@ test("Container service", async () => {
         const container = containers[i % containers.length];
         runExec(container, ["sh", "-c", 'while sleep 3600; do :; done']).catch(console.error);
     }
-    const loadMap = await dockerService.getExecLoad();
-    Array.from(loadMap.values()).forEach(value => {
-        expect(value).toEqual(2);
-    });
+    expect(await waitForTotalExecLoad(dockerService, 8)).toEqual(8);
+    const execLoad = await dockerService.getExecLoad();
+    for (const [containerId, load] of execLoad.entries()) {
+        expect(load).toEqual(2);
+    }
     await dockerService.remove();
     await dockerService.waitRemoved();
 });
